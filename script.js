616a05974ca6674d183dffa40ab6f50ae7b36d1e
rupertforbes <127436772+rupertforbes@users.noreply.github.com>
2023-10-24 16:38:20 +0100

    Added contact me modal for large resolutions and web page for small resolutions

---
+++ script.js
@@ -0,0 +1,16 @@
+let modal = document.querySelector(".modal");
+console.log(modal);
+let btn = document.getElementById("myBtn");
+console.log(btn);
+let span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
